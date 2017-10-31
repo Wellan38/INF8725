eaf60f761b0928fd3b98dd2c826958b0482be1bb
Juliette Tibayrenc <jtib@ntymail.com>
2017-10-31 10:36:20 -0400

    image filtre q4.5

--- TP2/Exercice4.m
+++ TP2/Exercice4.m
@@ -4,65 +4,72 @@ clear all;
 clc;
 close all;
 
-pkg load image
+%pkg load image
 
 %% Question 1
 % Chargement de l'image
-[img, map] = imread('maillot.png');
+[img,] = imread('maillot.png');
 
 % TFD
 tfd = fftshift(fft2(img));
 
 % Affichage
+Lx = size(tfd,1);
+Ly = size(tfd,2);
 imshow(img)
-%figure()
-%imshow(tfd) % Note : utiliser abs donne une image blanche
-%figure()
-%plot(abs(tfd))
+figure()
+imshow(log(1+abs(tfd/(Lx*Ly)))) % Normalisation et abs
 
 %% Question 2
-% Le col correspond au premier pic.
-% Le torse correspond au deuxi√®me.
-% La manche gauche (de face, la manche droite du porteur) correspond au troisi√®me pic.
-% La manche droite correspond au quatri√®me.
-% La poche correspond au dernier pic.
-% TODO: sch√©ma (comment ?)
+% Le col correspond au centre.
+% Le torse correspond ‡ la partie horizontale (puisqu'il prÈsente des rayures verticales).
+% La manche gauche (de face, la manche droite du porteur) correspond ‡ la diagonale des (-+/+-).
+% La manche droite correspond ‡ la diagonale (--/++).
+% La poche correspond ‡ la partie verticale.
+% TODO: schema
+
 
 %% Question 3
-% Cr√©ation du filtre
-H = fspecial('gaussian', size(tfd), 25); % 25 semble convenir
+% CrÈation du filtre
+H = fspecial('gaussian', size(tfd), 20); % 20 semble convenir
 % Application
 tfd_passe_bas = (tfd.*H)/(max(max(H)));
-% Affichage de l'image transform√©e
+% Affichage de l'image transformÈe
 img_passe_bas = uint8(abs(ifft2(ifftshift(tfd_passe_bas))));
 figure;
 imshow(img_passe_bas);
 title('Image filtr√©e (filtre gaussien passe-bas)')
 
 %% Question 4
-% On obtient un filtre passe-haut √† partir d'un filtre passe-bas.
-H_ph = 1 - fspecial('gaussian', size(tfd), 100); % probably not a gaussian
+% On obtient un filtre passe-haut ‡ partir d'un filtre passe-bas.
+H_ph = 1 - fspecial('gaussian', size(tfd), 0.5);
 % Application
-tfd_passe_haut = (tfd.*H_ph)/(max(max(H)));
-% Affichage de l'image transform√©e
+tfd_passe_haut = ((tfd.*H_ph)/(min(min(H_ph))));
+% Affichage de l'image transformÈe
 img_passe_haut = uint8(abs(ifft2(ifftshift(tfd_passe_haut))));
 figure;
 imshow(img_passe_haut);
-title('Image filtr√©e (filtre passe-haut)')
+title('Image filtrÈe (filtre passe-haut)')
 
 %% Question 5
+% Creation du filtre
+H_manches = load('filtre_ph_test.png');
+H_manches = rgb2gray(H_manches);
+H_manches = double([[H_manches ones(548,4)];ones(3,777)]);
+% Application
 
+% Affichage
 %% Question 6
 
 %% Question 7
-% Un filtre de Butterworth est proche d'un filtre id√©al (√©limination plus ou moins pr√©cise selon l'ordre du filtre de tout ce qui vient apr√®s la coupure ; traitement uniforme des fr√©quences venant avant la coupure) et l'approxime de mieux en mieux au fur et √† mesure que l'on augmente son ordre.
-% Il ne s'agit cependant pas d'un filtre id√©al, seulement d'une tr√®s bonne approximation.
+% Un filtre de Butterworth est proche d'un filtre idÈal (√©limination plus ou moins pr√©cise selon l'ordre du filtre de tout ce qui vient apr√®s la coupure ; traitement uniforme des fr√©quences venant avant la coupure) et l'approxime de mieux en mieux au fur et √† mesure que l'on augmente son ordre.
+% Il ne s'agit cependant pas d'un filtre idÈal, seulement d'une trËs bonne approximation.
 
 %% Question 8
-% La composante continue de l'image est filtr√©e et l'image contient des valeurs n√©gatives/nulles √† l'affichage.
+% La composante continue de l'image est filtrÈe et l'image contient des valeurs nÈgatives/nulles ‡ l'affichage.
 
 %% Question 9
 % La question n'a pas de sens dans le contexte de l'exercice.
 % TODO: demander quel est l'ordre pour le maillot.
 
-ginput();
+%ginput();
